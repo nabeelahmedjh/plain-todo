2727dcbfaa5c6c899c6c6d76ca2a9fd03791da74
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-08-25 15:05:45 +0500

    some old changes

--- plain_todo/plain_todo/settings.py
+++ plain_todo/plain_todo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,9 +81,24 @@ WSGI_APPLICATION = 'plain_todo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'USER': 'nabeel',
+        'PASSWORD': 'cs502002.',
+        'HOST': 'todotestserver.mysql.database.azure.com',
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # 'ssl': {
+            #     'ca': '{other-ca-cert filename}',
+            # },
+        }  
     }
 }
 
